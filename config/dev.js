0be49a63bc29bb4c83e55f855f742f0c7abab232
Phanindra <phani1716@gmail.com>
2018-06-28 21:09:00 +0530

    Final commit

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,7 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://Phanindra16:mahiroger3@ds119988.mlab.com:19988/blog_dev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: 'AKIAI3ZPBJ37ORMOPH4Q',
+  secretAccessKey: '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'
 };
